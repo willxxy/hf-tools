a4f7611834bdfefae942f2e8bb254b85e79fa070
willxxy <90741489+willxxy@users.noreply.github.com>
2024-03-24 17:40:46 -0400

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -1,5 +1,6 @@
 from transformers import AutoTokenizer AutoModel, BitsAndBytesConfig
 from peft import LoraConfig, TaskType, get_peft_model
+
 tokenizer = AutoTokenizer('path/to/checkpoint')
 model = AutoModel('path/to/checkpoint')
 
@@ -14,28 +15,3 @@ tokenizer.add_tokens(custom_tokens)
 
 ### RESIZES MODEL TOKEN EMBEDDINGS
 model.resize_token_embeddings(len(tokenizer))
-
-
-### LORA CONFIGS
-lora_config = LoraConfig(
-        r=8,
-        target_modules=["q_proj", "o_proj", 'k_proj', 'v_proj', 
-                        'gate_proj', 'up_proj', 'down_proj'],
-        bias = 'none',
-        lora_dropout=0.01,
-        task_type = TaskType.CAUSAL_LM,
-        )
-
-### BYTESANDBINARIES CONFIGS
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.bfloat16
-    )
-
-### ADDING CACHE PATH TO MODEL
-tokenizer = AutoTokenizer('path/to/checkpoint', cache_dir = 'path/to/cache')
-model = AutoModel('path/to/checkpoint', cache_dir = 'path/to/cache')
-
-### ADDING BNB
