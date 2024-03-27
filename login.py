31b56843e7006bd2d6bc60236e2999544b4237da
willxxy <90741489+willxxy@users.noreply.github.com>
2024-03-27 17:01:09 -0400

    Update login.py

--- login.py
+++ login.py
@@ -1,1 +1,8 @@
 
+### PRIVATE MODELS ACCESS witgh API KEY
+print('Loading API key')
+with open('./api_keys.txt', 'r') as file:
+    file_contents = file.readlines()
+api_key = file_contents[0]
+
+login(token = api_key)
